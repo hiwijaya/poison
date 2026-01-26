5ca76c7d79444e655c43cdf6eee6d97007f7db0e
Happy <happyindra@korindo.co.id>
2026-01-26 16:04:38 +0700

    fix: semantic tag post title

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -13,10 +13,10 @@ export default function PostCard({ post }: PostCardProps) {
       <Image className="w-full xs:w-52 sm:w-60 rounded-2xl object-contain" 
         src={post.cover} alt={post.title} width={1024} height={576}/>
       <div className="min-w-0">
-        <time className="text-xs">{post.date}</time>
-        <Link href={`/blog/${post.slug}`}>
-          <h2 className="text-lg font-bold text-highlight truncate">{post.title}</h2>
-        </Link>
+        <time dateTime={post.date} className="text-xs">{post.date}</time>
+        <h2 className="text-lg font-bold text-highlight truncate">
+          <Link href={`/blog/${post.slug}`}>{post.title}</Link>
+        </h2>
         <p className="text-xs line-clamp-2 my-2">{post.excerpt}</p>
         <Link href={`/blog/${post.slug}`} className="flex flex-row text-xs text-primary">
           Read more
