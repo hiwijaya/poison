a8d1953a63a285348c6168f2d2f97eb114c4039e
Happy <happyindra@korindo.co.id>
2026-01-26 13:30:26 +0700

    feat: responsive PostCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,18 @@
+import Link from "next/link";
 import PostCard from "@/components/PostCard";
 import RedirectButton from "@/components/RedirectButton";
 import { getRecentPosts } from "@/lib/api";
+
 
 export default function Home() {
 
   const renderRecentPosts = () => (
     <div className="my-16">
-      <div className="mb-6">RECENT POSTS</div>
-      <div className="grid grid-cols-1 gap-8">
-        {
-          getRecentPosts().map(post => (
-            <PostCard key={post.slug} post={post}/>
-          ))
-        }
+      <div className="text-highlight mb-6">RECENT POSTS</div>
+      <div className="grid grid-cols-1 gap-8 xs:gap-6 mb-6">
+        { getRecentPosts().map(post => <PostCard key={post.slug} post={post}/>) }
       </div>
+      <Link href="/blog" className="inline-flex text-primary">More posts</Link>
     </div>
   );
 
@@ -25,9 +24,7 @@ export default function Home() {
         it blends minimal design with bold personality—letting your work speak louder than the noise. 
         Built for developers who don't just build products, but leave a lasting bite.
       </p>
-      <RedirectButton href="https://github.com/hiwijaya/poison">
-        START NOW
-      </RedirectButton>
+      <RedirectButton href="https://github.com/hiwijaya/poison">START NOW</RedirectButton>
 
       {renderRecentPosts()}
     </div>
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,5 +1,6 @@
 import { PostMeta } from "@/lib/types";
 import Image from "next/image";
+import Link from "next/link";
 
 
 interface PostCardProps {
@@ -8,18 +9,18 @@ interface PostCardProps {
 
 export default function PostCard({ post }: PostCardProps) {
   return(
-    <article className="flex flex-col xs:flex-row space-y-2 xs:space-x-4">
-      <Image className="w-full xs:w-48 rounded-2xl object-contain" src={post.cover} alt={post.title} 
-        width={1024} height={576}/>
+    <article className="flex flex-col xs:flex-row space-y-2 xs:space-y-0 xs:space-x-4">
+      <Image className="w-full xs:w-52 sm:w-60 rounded-2xl object-contain" 
+        src={post.cover} alt={post.title} width={1024} height={576}/>
       <div className="min-w-0">
-        <h2 className="font-bold text-highlight cursor-pointer truncate mb-2">{post.title}</h2>
         <time className="text-xs">{post.date}</time>
-        <span className="mx-2">&middot;</span>
-        <ul className="inline-flex space-x-2">
-          {post.tags.map((tag, i) => (
-            <li key={i} className="text-xs px-4 py-1 rounded-full]">{tag}</li>
-          ))}
-        </ul>
+        <Link href={`/blog/${post.slug}`}>
+          <h2 className="text-lg font-bold text-highlight truncate">{post.title}</h2>
+        </Link>
+        <p className="text-xs line-clamp-2 my-2">{post.excerpt}</p>
+        <Link href={`/blog/${post.slug}`} className="flex flex-row text-xs text-primary">
+          Read more
+        </Link>
       </div>
     </article>
   );
