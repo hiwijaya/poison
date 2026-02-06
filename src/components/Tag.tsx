0715e589c019e04e923f333ee74c6f3123348b2a
Happy <happyindra@korindo.co.id>
2026-02-06 10:26:58 +0700

    chore: tag hover color

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -7,6 +7,6 @@ interface TagProps {
 export default function Tag({ name }: TagProps) {
   return (
     <Link href={`/tag/${name}`}
-      className="bg-[#242E28] text-foreground! text-xs px-4 py-1.5 rounded-full">{name}</Link>
+      className="bg-[#242E28] text-foreground! hover:text-primary! text-xs px-4 py-1.5 rounded-full">{name}</Link>
   );
 }
